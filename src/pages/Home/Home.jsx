7ca0d130068059bda07fc316da7a077ae63ee2f2
Xiodanny-Vasquez <xiodanny.vasquez@soy.sena.edu.co>
2024-10-31 11:47:22 -0500

    agregar Ultima seccion

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HeroSection from './Hero/HeroSection'
 import NavBar from './Navbar/NavBar'
+import UltimaSection from './Ultimo/UltimaSection'
+
 
 function Home() {
   return (
     <div>
         <NavBar/>
         <HeroSection/>
+        <UltimaSection/>
     </div>
   )
 }
