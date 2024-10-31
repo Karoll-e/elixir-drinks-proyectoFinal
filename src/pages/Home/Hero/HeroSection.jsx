02569bac160891d1c2b02cb8a18e7d79343bd649
Karoll Escalante <karollescalante02@gmail.com>
2024-10-31 15:40:19 -0500

    Actualizar contenido del HeroSection en la página de inicio

--- src/pages/Home/Hero/HeroSection.jsx
+++ src/pages/Home/Hero/HeroSection.jsx
@@ -4,15 +4,10 @@ import { Button, Container } from "react-bootstrap";
 
 function HeroSection() {
   return (
-    <div
-      className="hero-section w-auto text-white d-flex flex-column justify-content-center align-items-center "
-    >
+    <div className="hero-section w-auto text-white d-flex flex-column justify-content-center align-items-center ">
       <Container>
         <h1 className="">Elixir</h1>
-        <p className="lead fs-4">
-          Explora el arte de la mixología con nuestros exclusivos cocteles
-          artesanales.
-        </p>
+        <p className="lead fs-4">Donde cada trago cuenta una historia</p>
         <Button variant="secondary" size="ms">
           Explorar nuestros cocteles
         </Button>
