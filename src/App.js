c7effe3d2d7809cd554aaee88bc2724ec007c3e2
Karoll Escalante <karollescalante02@gmail.com>
2024-10-31 17:36:06 -0500

    Implementar enrutamiento con react-router-dom y mejorar NavBar para la navegación

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@
 import Home from "./pages/Home/Home";
 import './customTheme.css'
 import Contact from "./pages/Contact/Contact";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./pages/Home/Navbar/NavBar";
+
+
 function App() {
   return (
-    <div>
-      <Home />
-      <Contact />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre-nosotros" element={""} />
+            <Route path="/productos" element={""} />
+            <Route path="/contacto" element={<Contact />} />
+            <Route path="/cart" element={""} />
+            <Route path="/account" element={""} />
+            {/* Add a catch-all route for 404 pages */}
+            <Route path="*" element={
+              <div className="d-flex justify-content-center align-items-center" style={{ minHeight: "80vh" }}>
+                <h2>404 - Página no encontrada</h2>
+              </div>
+            } />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import HeroSection from './Hero/HeroSection'
-import NavBar from './Navbar/NavBar'
 import UltimaSection from './Ultimo/UltimaSection'
 
 
 function Home() {
   return (
     <div>
-        <NavBar/>
         <HeroSection/>
         <UltimaSection/>
     </div>
