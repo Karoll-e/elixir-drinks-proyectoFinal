a8138f3b63a6e6579ae9c66d69a0911c81e8c16c
Karoll Escalante <karollescalante02@gmail.com>
2024-10-31 18:53:18 -0500

    Refactorización de los estilos de HeroSection y actualización del texto para mayor claridad.

--- src/pages/Home/Hero/HeroSection.jsx
+++ src/pages/Home/Hero/HeroSection.jsx
@@ -7,9 +7,7 @@ function HeroSection() {
     <div className="hero-section w-auto text-white d-flex flex-column justify-content-center align-items-center ">
       <Container>
         <h1 className="">Elixir</h1>
-        <p className="lead fs-4">
-          Donde cada trago cuenta una historia.
-        </p>
+        <p className="lead fs-4">Donde cada trago cuanta una historia.</p>
         <Button className="btn" variant="secondary" size="lg">
           Explora nuestros cocteles exclusivos
         </Button>
