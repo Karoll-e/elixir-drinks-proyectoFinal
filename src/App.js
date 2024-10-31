7482ade21d17e67fe3e4e131c5911231cc7ffc70
Karoll Escalante <karollescalante02@gmail.com>
2024-10-31 15:40:27 -0500

    Añadir componente CocktailList y detalles de cócteles, incluyendo filtros y carga de datos desde la API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 //import Cocktails from "./pages/Cocteles/Cocktails";
 import Home from "./pages/Home/Home";
 import './customTheme.css'
+import CocktailList from "./pages/CocktailDetails/CocktailList";
+
+
 function App() {
   return (
     <div>
       <Home />
+      <CocktailList />
       
     </div>
   );
