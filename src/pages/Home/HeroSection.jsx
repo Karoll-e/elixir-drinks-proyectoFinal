f27f836688fb0bd305bb3c13dbf1d093ac6875a8
Karoll Escalante <karollescalante02@gmail.com>
2024-10-30 19:01:56 -0500

    Añadir componente HeroSection con estilos y Navbar

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./Hero.css";
+import { Button, Container } from "react-bootstrap";
 
 function HeroSection() {
   return (
-    <div>HeroSection</div>
-  )
+    <div
+      className="hero-section w-auto text-white d-flex flex-column justify-content-center align-items-center "
+    >
+      <Container>
+        <h1 className="">Elixir</h1>
+        <p className="lead fs-4">
+          Explora el arte de la mixología con nuestros exclusivos cocteles
+          artesanales.
+        </p>
+        <Button variant="secondary" size="ms">
+          Explorar nuestros cocteles
+        </Button>
+      </Container>
+    </div>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
--- src/pages/Home/NavBar.jsx
+++ src/pages/Home/NavBar.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+// Navbar.js
+import React from "react";
+import { Navbar, Nav, Container, NavItem } from "react-bootstrap";
+import { ShoppingCart, User } from "lucide-react";
+import logoElixir from "../../assets/logo-elixir.png";
 
-function NavBar() {
+const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        {/* Logo a la izquierda */}
+        <Navbar.Brand href="#home">
+          <img src={logoElixir} alt="" style={{ height: "40px" }} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="justify-content-center"
+        >
+          <Nav
+            className="mx-auto"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "50%",
+            }}
+          >
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#about">Sobre Nosotros</Nav.Link>
+            <Nav.Link href="#products">Productos</Nav.Link>
+            <Nav.Link href="#contact">Contacto</Nav.Link>
+          </Nav>
 
-export default NavBar
+          {/* Íconos a la derecha */}
+          <Nav className="ms-auto">
+            <NavItem>
+              <Nav.Link href="#cart">
+                <ShoppingCart color="white" size={20} />
+              </Nav.Link>
+            </NavItem>
+            <NavItem>
+              <Nav.Link href="#account">
+                <User color="white" size={20} />
+              </Nav.Link>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
