57fd226fdf9429d18cde8508c5854a2d472207b1
Karoll Escalante <karollescalante02@gmail.com>
2024-11-02 00:36:38 -0500

    Agregar componentes CocktailList y HeroProducts con estilos y funcionalidad para mostrar cócteles

---
+++ src/pages/Productos/HeroProducts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './HeroProducts.css'
+import Image1 from './imagen.jpeg'
+
+function HeroProducts() {
+    return (
+        <div className="hero-product slide" >
+          <h1 className="">COCTELES</h1>
+        </div>
+      );
+}
+
+export default HeroProducts;
