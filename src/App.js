be253fd6ba2e3f45d4924955d6192676bd8d8d9c
sharymhub <sharaynicollew@gmail.com>
2024-11-01 22:08:43 -0500

    Seccion About
    Cree una carpeta llamada About, la cual contiene algunas sub carpetas con la división de cada parte del About y todos estos componentes se reunen en el componente padre (About), y le agregue responsive a esta seccion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 //import Cocktails from "./pages/Cocteles/Cocktails";
 import Home from "./pages/Home/Home";
 import './customTheme.css'
+import About from "./pages/About/About";
 function App() {
   return (
     <div>
-      <Home />
+      <Home/>
+      <About/>
       
     </div>
   );
