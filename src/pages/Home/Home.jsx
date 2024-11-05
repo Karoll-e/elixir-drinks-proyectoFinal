23ef6ea395b555f9eb05fe48a152d14c72799afd
Ana Milena Vasquez Tovar <amvasquez742@soy.sena.edu.co>
2024-11-05 17:59:31 -0500

    Se agregar la parte del FooterSection.jsx, Footer.css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,8 @@ import Main from './Main/Main'
 import HeroSection from './Hero/HeroSection'
 import UltimaSection from './Ultimo/UltimaSection'
 import IconsSection  from './IconsSection/IconsSection'
+import FooterSection from './Footer/FooterSection'
+
 function Home() {
   return (
     <div>
@@ -12,6 +14,7 @@ function Home() {
         <IconsSection/>
         <Main/>
         <UltimaSection/>
+        <FooterSection/>
     </div>
   )
 }
