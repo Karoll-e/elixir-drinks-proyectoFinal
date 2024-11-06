10d01d002abf15727b696cb387b832ae673b9a70
Karoll Escalante <karollescalante02@gmail.com>
2024-11-06 17:13:51 -0500

    modificar precio en vista detallada de cocteles

--- src/components/CoctelCard.jsx
+++ src/components/CoctelCard.jsx
@@ -2,37 +2,44 @@ import React from "react";
 import "./CoctelCard.css";
 import { useCart } from "../pages/Shopping-cart/Hooks/useCart";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 const CoctelCard = ({ cocktail }) => {
+  
   const navigate = useNavigate();
   const { addToCart } = useCart();
 
   const handleClick = () => {
-    navigate(`/cocktail/${cocktail.idDrink}`);
+    navigate(`/cocktail/${cocktail.idDrink}`, {
+      state: { price: cocktail.price }
+    });
   };
 
   return (
-    <div className="cont" onClick={handleClick}>
-      <div className="product-card">
-        <div className="product-card__image">
+    <div className="cont">
+      <div className="product-card" >
+        <div className="product-card__image" onClick={handleClick}>
           <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
         </div>
         <div className="product-card__info">
-          <h2 className="product-card__title text-truncate mb-3">
+          <h2 className="product-card__title text-truncate mb-3" onClick={handleClick}>
             {cocktail.strDrink}
           </h2>
           <div className="product-card__price-row">
-            <span className="product-card__price">
-              {cocktail.price ? cocktail.price : "N/A"}
+            <span className="product-card__price" onClick={handleClick}>
+              {cocktail.price ? cocktail.price : ""}
             </span>
-            <button
-              className="product-card__btn"
-              onClick={(e) => {
-                e.stopPropagation().addToCart(cocktail);
-              }}
-            >
-              Add to Cart
-            </button>
+            <Link to="/cart">
+              <button
+                className="product-card__btn"
+                onClick={() => {
+                  addToCart(cocktail);
+                }}
+              >
+                Add to Cart
+              </button>
+            </Link>
           </div>
         </div>
       </div>
